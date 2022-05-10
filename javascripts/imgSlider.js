180d1a28ec67d92c1c16b4fefb4fe506de018446
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2022-05-10 17:30:31 +0200

    Create imgSlider.js

---
+++ javascripts/imgSlider.js
@@ -0,0 +1,9 @@
+
+function scrolll() {
+    let left = document.querySelector(".scroll-images");
+    left.scrollBy(350,0)
+}
+function scrollright() {
+    let right = document.querySelector(".scroll-images");
+    right.scrollBy(-350,0)
+}
